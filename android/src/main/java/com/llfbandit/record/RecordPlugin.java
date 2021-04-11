4b48d699b641fe0b090dd4ec504b03240788be63
v7lin <v7lin@qq.com>
2021-04-11 18:35:06 +0800

    Background Isolate

--- android/src/main/java/com/llfbandit/record/RecordPlugin.java
+++ android/src/main/java/com/llfbandit/record/RecordPlugin.java
@@ -1,82 +1,155 @@
 package com.llfbandit.record;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.os.Build;
+
 import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
 
 import io.flutter.embedding.engine.plugins.FlutterPlugin;
 import io.flutter.embedding.engine.plugins.activity.ActivityAware;
 import io.flutter.embedding.engine.plugins.activity.ActivityPluginBinding;
-import io.flutter.plugin.common.BinaryMessenger;
+import io.flutter.plugin.common.MethodCall;
 import io.flutter.plugin.common.MethodChannel;
+import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
+import io.flutter.plugin.common.MethodChannel.Result;
+import io.flutter.plugin.common.PluginRegistry;
 
 /**
  * RecordPlugin
  */
-public class RecordPlugin implements FlutterPlugin, ActivityAware {
-  /// The MethodChannel that will the communication between Flutter and native Android
-  private MethodChannel channel;
-  /// Our call handler
-  private MethodCallHandlerImpl handler;
-  private FlutterPluginBinding pluginBinding;
-  private ActivityPluginBinding activityBinding;
+public class RecordPlugin implements FlutterPlugin, ActivityAware, PluginRegistry.RequestPermissionsResultListener, MethodCallHandler {
+    private static final int RECORD_AUDIO_REQUEST_CODE = RecordPlugin.class.hashCode() + 43;
+    private MethodChannel channel;
 
-  /////////////////////////////////////////////////////////////////////////////
-  /// FlutterPlugin
-  /////////////////////////////////////////////////////////////////////////////
-  @Override
-  public void onAttachedToEngine(@NonNull FlutterPluginBinding binding) {
-    pluginBinding = binding;
-  }
+    private Context applicationContext;
+    private Recorder recorder;
 
-  @Override
-  public void onDetachedFromEngine(@NonNull FlutterPluginBinding binding) {
-    pluginBinding = null;
-  }
-  /// END FlutterPlugin
-  /////////////////////////////////////////////////////////////////////////////
+    private ActivityPluginBinding activityPluginBinding;
+    private Result pendingPermResult;
 
+    /// --- FlutterPlugin
+    @Override
+    public void onAttachedToEngine(@NonNull FlutterPluginBinding binding) {
+        channel = new MethodChannel(binding.getBinaryMessenger(), "com.llfbandit.record");
+        channel.setMethodCallHandler(this);
 
-  /////////////////////////////////////////////////////////////////////////////
-  /// ActivityAware
-  /////////////////////////////////////////////////////////////////////////////
-  @Override
-  public void onAttachedToActivity(@NonNull ActivityPluginBinding binding) {
-    activityBinding = binding;
+        applicationContext = binding.getApplicationContext();
+        recorder = new Recorder();
+    }
 
-    startPlugin(pluginBinding.getBinaryMessenger(), binding);
-  }
+    @Override
+    public void onDetachedFromEngine(@NonNull FlutterPluginBinding binding) {
+        channel.setMethodCallHandler(null);
+        channel = null;
 
-  @Override
-  public void onDetachedFromActivityForConfigChanges() {
-    onDetachedFromActivity();
-  }
+        applicationContext = null;
+        recorder.close();
+        recorder = null;
+    }
 
-  @Override
-  public void onReattachedToActivityForConfigChanges(@NonNull ActivityPluginBinding binding) {
-    onAttachedToActivity(binding);
-  }
+    // --- ActivityAware
 
-  @Override
-  public void onDetachedFromActivity() {
-    stopPlugin();
-  }
+    @Override
+    public void onAttachedToActivity(@NonNull ActivityPluginBinding binding) {
+        activityPluginBinding = binding;
+        activityPluginBinding.addRequestPermissionsResultListener(this);
+    }
 
-  private void startPlugin(BinaryMessenger messenger, ActivityPluginBinding binding) {
+    @Override
+    public void onDetachedFromActivityForConfigChanges() {
+        onDetachedFromActivity();
+    }
 
-    handler = new MethodCallHandlerImpl(binding.getActivity());
-    channel = new MethodChannel(messenger, "com.llfbandit.record");
-    channel.setMethodCallHandler(handler);
+    @Override
+    public void onReattachedToActivityForConfigChanges(@NonNull ActivityPluginBinding binding) {
+        onAttachedToActivity(binding);
+    }
 
-    binding.addRequestPermissionsResultListener(handler);
-  }
+    @Override
+    public void onDetachedFromActivity() {
+        activityPluginBinding.removeRequestPermissionsResultListener(this);
+        activityPluginBinding = null;
+    }
 
-  private void stopPlugin() {
-    activityBinding.removeRequestPermissionsResultListener(handler);
-    activityBinding = null;
-    channel.setMethodCallHandler(null);
-    handler.close();
-    handler = null;
-    channel = null;
-  }
-  /// END ActivityAware
-  /////////////////////////////////////////////////////////////////////////////
+    // --- RequestPermissionsResultListener
+
+    @Override
+    public boolean onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        if (requestCode == RECORD_AUDIO_REQUEST_CODE) {
+            if (pendingPermResult != null) {
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    pendingPermResult.success(true);
+                } else {
+                    pendingPermResult.error("-2", "Permission denied", null);
+                }
+                pendingPermResult = null;
+                return true;
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public void onMethodCall(@NonNull MethodCall call, @NonNull Result result) {
+        switch (call.method) {
+            case "start":
+                recorder.start(
+                        (String) call.argument("path"),
+                        (int) call.argument("encoder"),
+                        (int) call.argument("bitRate"),
+                        (double) call.argument("samplingRate"),
+                        result);
+                break;
+            case "stop":
+                recorder.stop(result);
+                break;
+            case "pause":
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+                    recorder.pause(result);
+                }
+                break;
+            case "resume":
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+                    recorder.resume(result);
+                }
+                break;
+            case "isPaused":
+                recorder.isPaused(result);
+                break;
+            case "isRecording":
+                recorder.isRecording(result);
+                break;
+            case "hasPermission":
+                hasPermission(result);
+                break;
+            default:
+                result.notImplemented();
+                break;
+        }
+    }
+
+    private void hasPermission(@NonNull Result result) {
+        if (!isPermissionGranted()) {
+            pendingPermResult = result;
+            askForPermission();
+        } else {
+            result.success(true);
+        }
+    }
+
+    private boolean isPermissionGranted() {
+        int result = ActivityCompat.checkSelfPermission(activityPluginBinding.getActivity(), Manifest.permission.RECORD_AUDIO);
+        return result == PackageManager.PERMISSION_GRANTED;
+    }
+
+    private void askForPermission() {
+        ActivityCompat.requestPermissions(
+                activityPluginBinding.getActivity(),
+                new String[]{Manifest.permission.RECORD_AUDIO},
+                RECORD_AUDIO_REQUEST_CODE
+        );
+    }
 }
--- android/src/main/java/com/llfbandit/record/Recorder.java
+++ android/src/main/java/com/llfbandit/record/Recorder.java
@@ -10,151 +10,151 @@ import androidx.annotation.RequiresApi;
 import io.flutter.plugin.common.MethodChannel.Result;
 
 class Recorder {
-  private static final String LOG_TAG = "Record";
+    private static final String LOG_TAG = "Record";
 
-  private boolean isRecording = false;
-  private boolean isPaused = false;
+    private boolean isRecording = false;
+    private boolean isPaused = false;
 
-  private MediaRecorder recorder = null;
+    private MediaRecorder recorder = null;
 
-  void start(
-          @NonNull String path,
-          int encoder,
-          int bitRate,
-          double samplingRate,
-          @NonNull Result result
-  ) {
-    stopRecording();
+    void start(
+            @NonNull String path,
+            int encoder,
+            int bitRate,
+            double samplingRate,
+            @NonNull Result result
+    ) {
+        stopRecording();
 
-    Log.d(LOG_TAG, "Start recording");
+        Log.d(LOG_TAG, "Start recording");
 
-    recorder = new MediaRecorder();
-    recorder.setAudioSource(MediaRecorder.AudioSource.MIC);
-    recorder.setAudioEncodingBitRate(bitRate);
-    recorder.setAudioSamplingRate((int) samplingRate);
-    recorder.setOutputFormat(getOutputFormat(encoder));
-    // must be set after output format
-    recorder.setAudioEncoder(getEncoder(encoder));
-    recorder.setOutputFile(path);
+        recorder = new MediaRecorder();
+        recorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+        recorder.setAudioEncodingBitRate(bitRate);
+        recorder.setAudioSamplingRate((int) samplingRate);
+        recorder.setOutputFormat(getOutputFormat(encoder));
+        // must be set after output format
+        recorder.setAudioEncoder(getEncoder(encoder));
+        recorder.setOutputFile(path);
 
-    try {
-      recorder.prepare();
-      recorder.start();
-      isRecording = true;
-      isPaused = false;
-      result.success(null);
-    } catch (Exception e) {
-      recorder.release();
-      recorder = null;
-      result.error("-1", "Start recording failure", e.getMessage());
-    }
-  }
-
-  void stop(@NonNull Result result) {
-    stopRecording();
-    result.success(null);
-  }
-
-  @RequiresApi(api = Build.VERSION_CODES.N)
-  void pause(@NonNull Result result) {
-    pauseRecording();
-    result.success(null);
-  }
-
-  @RequiresApi(api = Build.VERSION_CODES.N)
-  void resume(@NonNull Result result) {
-    resumeRecording();
-    result.success(null);
-  }
-
-  void isRecording(@NonNull Result result) {
-    result.success(isRecording);
-  }
-
-  void isPaused(@NonNull Result result) {
-    result.success(isPaused);
-  }
-
-  void close() {
-    stopRecording();
-  }
-
-  private void stopRecording() {
-    if (recorder != null) {
-      try {
-        if (isRecording || isPaused) {
-          Log.d(LOG_TAG, "Stop recording");
-          recorder.stop();
+        try {
+            recorder.prepare();
+            recorder.start();
+            isRecording = true;
+            isPaused = false;
+            result.success(null);
+        } catch (Exception e) {
+            recorder.release();
+            recorder = null;
+            result.error("-1", "Start recording failure", e.getMessage());
         }
-      } catch (IllegalStateException ex) {
-        // Mute this exception since 'isRecording' can't be 100% sure
-      } finally {
-        recorder.release();
-        recorder = null;
-      }
     }
 
-    isRecording = false;
-    isPaused = false;
-  }
+    void stop(@NonNull Result result) {
+        stopRecording();
+        result.success(null);
+    }
 
-  @RequiresApi(api = Build.VERSION_CODES.N)
-  private void pauseRecording() {
-    if (recorder != null) {
-      try {
-        if (isRecording) {
-          Log.d(LOG_TAG, "Pause recording");
-          recorder.pause();
-          isPaused = true;
+    @RequiresApi(api = Build.VERSION_CODES.N)
+    void pause(@NonNull Result result) {
+        pauseRecording();
+        result.success(null);
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.N)
+    void resume(@NonNull Result result) {
+        resumeRecording();
+        result.success(null);
+    }
+
+    void isRecording(@NonNull Result result) {
+        result.success(isRecording);
+    }
+
+    void isPaused(@NonNull Result result) {
+        result.success(isPaused);
+    }
+
+    void close() {
+        stopRecording();
+    }
+
+    private void stopRecording() {
+        if (recorder != null) {
+            try {
+                if (isRecording || isPaused) {
+                    Log.d(LOG_TAG, "Stop recording");
+                    recorder.stop();
+                }
+            } catch (IllegalStateException ex) {
+                // Mute this exception since 'isRecording' can't be 100% sure
+            } finally {
+                recorder.release();
+                recorder = null;
+            }
         }
-      } catch (IllegalStateException ex) {
-        Log.d(LOG_TAG, "Did you call pause() before before start() or after stop()?\n" + ex.getMessage());
-      }
-    }
-  }
 
-  @RequiresApi(api = Build.VERSION_CODES.N)
-  private void resumeRecording() {
-    if (recorder != null) {
-      try {
-        if (isPaused) {
-          Log.d(LOG_TAG, "Resume recording");
-          recorder.resume();
-          isPaused = false;
+        isRecording = false;
+        isPaused = false;
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.N)
+    private void pauseRecording() {
+        if (recorder != null) {
+            try {
+                if (isRecording) {
+                    Log.d(LOG_TAG, "Pause recording");
+                    recorder.pause();
+                    isPaused = true;
+                }
+            } catch (IllegalStateException ex) {
+                Log.d(LOG_TAG, "Did you call pause() before before start() or after stop()?\n" + ex.getMessage());
+            }
         }
-      } catch (IllegalStateException ex) {
-        Log.d(LOG_TAG, "Did you call resume() before before start() or after stop()?\n" + ex.getMessage());
-      }
-    }
-  }
-
-  private int getOutputFormat(int encoder) {
-    if (encoder == 3 || encoder == 4) {
-      return MediaRecorder.OutputFormat.THREE_GPP;
     }
 
-    return MediaRecorder.OutputFormat.MPEG_4;
-  }
-
-  // https://developer.android.com/reference/android/media/MediaRecorder.AudioEncoder
-  private int getEncoder(int encoder) {
-    switch (encoder) {
-      case 1:
-        return MediaRecorder.AudioEncoder.AAC_ELD;
-      case 2:
-        return MediaRecorder.AudioEncoder.HE_AAC;
-      case 3:
-        return MediaRecorder.AudioEncoder.AMR_NB;
-      case 4:
-        return MediaRecorder.AudioEncoder.AMR_WB;
-      case 5:
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
-          return MediaRecorder.AudioEncoder.OPUS;
-        } else {
-          Log.d(LOG_TAG, "OPUS codec is available starting from API 29.\nFalling back to AAC");
+    @RequiresApi(api = Build.VERSION_CODES.N)
+    private void resumeRecording() {
+        if (recorder != null) {
+            try {
+                if (isPaused) {
+                    Log.d(LOG_TAG, "Resume recording");
+                    recorder.resume();
+                    isPaused = false;
+                }
+            } catch (IllegalStateException ex) {
+                Log.d(LOG_TAG, "Did you call resume() before before start() or after stop()?\n" + ex.getMessage());
+            }
         }
-      default:
-        return MediaRecorder.AudioEncoder.AAC;
     }
-  }
+
+    private int getOutputFormat(int encoder) {
+        if (encoder == 3 || encoder == 4) {
+            return MediaRecorder.OutputFormat.THREE_GPP;
+        }
+
+        return MediaRecorder.OutputFormat.MPEG_4;
+    }
+
+    // https://developer.android.com/reference/android/media/MediaRecorder.AudioEncoder
+    private int getEncoder(int encoder) {
+        switch (encoder) {
+            case 1:
+                return MediaRecorder.AudioEncoder.AAC_ELD;
+            case 2:
+                return MediaRecorder.AudioEncoder.HE_AAC;
+            case 3:
+                return MediaRecorder.AudioEncoder.AMR_NB;
+            case 4:
+                return MediaRecorder.AudioEncoder.AMR_WB;
+            case 5:
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+                    return MediaRecorder.AudioEncoder.OPUS;
+                } else {
+                    Log.d(LOG_TAG, "OPUS codec is available starting from API 29.\nFalling back to AAC");
+                }
+            default:
+                return MediaRecorder.AudioEncoder.AAC;
+        }
+    }
 }
