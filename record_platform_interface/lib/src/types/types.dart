869158b34ae604fa36216d0135b5c441d69eeed2
DomingoMG <domingomg97@gmail.com>
2022-06-25 00:17:50 +0100

    Device type has been added.

--- record_platform_interface/lib/src/types/types.dart
+++ record_platform_interface/lib/src/types/types.dart
@@ -1,2 +1,3 @@
 export 'package:record_platform_interface/src/types/amplitude.dart';
 export 'package:record_platform_interface/src/types/audio_encoder.dart';
+export 'package:record_platform_interface/src/types/device.dart';
