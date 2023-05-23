80a3237c2e9e7e071c2a7d114e1789d0bb2bc245
llfbandit <llfbandit@gmail.com>
2023-05-23 14:50:00 +0200

    feat: Add multi-instances support to Web platform.

--- record_web/lib/record_web.dart
+++ record_web/lib/record_web.dart
@@ -1,281 +1,107 @@
 import 'dart:async';
-import 'dart:html' as html;
 
-import 'package:flutter/foundation.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 import 'package:record_platform_interface/record_platform_interface.dart';
-import 'package:record_web/mime_types.dart';
+import 'package:record_web/recorder.dart';
 
-class RecordPluginWeb extends RecordPlatform {
+class RecordPluginWeb {
   static void registerWith(Registrar registrar) {
-    RecordPlatform.instance = RecordPluginWeb();
+    RecordPlatform.instance = RecordPluginWebWrapper();
   }
+}
 
-  // Media recorder object
-  html.MediaRecorder? _mediaRecorder;
-  // Media stream get from getUserMedia
-  html.MediaStream? _mediaStream;
-  // Audio data
-  List<html.Blob> _chunks = [];
-  // Completer to get data & stop events before `stop()` method ends
-  Completer<String>? _onStopCompleter;
-  StreamController<RecordState>? _stateStreamCtrl;
-  StreamController<List<int>>? _recordStreamCtrl;
+class RecordPluginWebWrapper extends RecordPlatform {
+  // recorders from recorderId
+  final _recorders = <String, Recorder>{};
 
   @override
-  Future<void> dispose() async {
-    _mediaRecorder?.stop();
-    _stateStreamCtrl?.close();
-    _recordStreamCtrl?.close();
-    _resetMediaRecorder();
+  Future<void> create(String recorderId) async {
+    _recorders[recorderId] = Recorder();
   }
 
   @override
-  Future<bool> hasPermission() async {
-    final mediaDevices = html.window.navigator.mediaDevices;
-    if (mediaDevices == null) return false;
+  Future<void> dispose(String recorderId) async {
+    final recorder = _getRecorder(recorderId);
+    await recorder.dispose();
 
-    try {
-      await mediaDevices.getUserMedia({'audio': true});
-      return true;
-    } catch (_) {
-      return false;
-    }
+    _recorders.remove(recorderId);
   }
 
   @override
-  Future<bool> isPaused() async {
-    return _mediaRecorder?.state == 'paused';
+  Future<bool> hasPermission(String recorderId) {
+    return _getRecorder(recorderId).hasPermission();
   }
 
   @override
-  Future<bool> isRecording() async {
-    return _mediaRecorder?.state == 'recording';
+  Future<bool> isPaused(String recorderId) {
+    return _getRecorder(recorderId).isPaused();
   }
 
   @override
-  Future<void> pause() async {
-    _mediaRecorder?.pause();
-
-    _updateState(RecordState.pause);
+  Future<bool> isRecording(String recorderId) {
+    return _getRecorder(recorderId).isRecording();
   }
 
   @override
-  Future<void> resume() async {
-    _mediaRecorder?.resume();
-
-    _updateState(RecordState.record);
+  Future<void> pause(String recorderId) {
+    return _getRecorder(recorderId).pause();
   }
 
   @override
-  Future<void> start(RecordConfig config, {required String path}) async {
-    return _start(config);
+  Future<void> resume(String recorderId) {
+    return _getRecorder(recorderId).resume();
   }
 
   @override
-  Future<Stream<List<int>>> startStream(RecordConfig config) async {
-    await _start(config);
-
-    _recordStreamCtrl = StreamController();
-    return _recordStreamCtrl!.stream;
+  Future<void> start(
+    String recorderId,
+    RecordConfig config, {
+    required String path,
+  }) {
+    return _getRecorder(recorderId).start(config, path: path);
   }
 
   @override
-  Future<String?> stop() async {
-    _onStopCompleter = Completer();
-
-    _mediaRecorder?.stop();
-
-    _updateState(RecordState.stop);
-
-    return _onStopCompleter?.future;
+  Future<Stream<List<int>>> startStream(
+    String recorderId,
+    RecordConfig config,
+  ) {
+    return _getRecorder(recorderId).startStream(config);
   }
 
   @override
-  Future<List<InputDevice>> listInputDevices() async {
-    final devices = <InputDevice>[];
-
-    final mediaDevices = html.window.navigator.mediaDevices;
-    try {
-      if (mediaDevices == null) {
-        _onError('enumerateDevices() not supported.');
-        return devices;
-      }
-
-      final deviceInfos = await mediaDevices.enumerateDevices();
-      for (var info in deviceInfos) {
-        if (info is html.MediaDeviceInfo &&
-            info.kind == 'audioinput' &&
-            info.deviceId != null &&
-            info.label != null) {
-          devices.add(InputDevice(id: info.deviceId!, label: info.label!));
-        }
-      }
-    } catch (error) {
-      _onError(error);
-    }
-
-    return devices;
-  }
-
-  Future<void> _start(RecordConfig config, [bool isStream = false]) async {
-    _mediaRecorder?.stop();
-    _resetMediaRecorder();
-
-    final constraints = {
-      'audio': true,
-      'audioBitsPerSecond': config.bitRate,
-      'bitsPerSecond': config.bitRate,
-      'sampleRate': config.samplingRate,
-      'channelCount': config.numChannels,
-      if (config.device != null) 'deviceId': {'exact': config.device!.id}
-    };
-
-    // Try to assign dedicated mime type.
-    // If contrainst isn't set, browser will record with its default codec.
-    final mimeType = _getSupportedMimeType(config.encoder);
-    if (mimeType != null) {
-      constraints['mimeType'] = mimeType;
-    }
-
-    try {
-      _mediaStream = await html.window.navigator.mediaDevices?.getUserMedia(
-        constraints,
-      );
-      if (_mediaStream != null) {
-        _onStart(_mediaStream!, isStream);
-      } else {
-        _log('Audio recording not supported.');
-      }
-    } catch (error) {
-      _onError(error);
-    }
-  }
-
-  void _onStart(html.MediaStream stream, [bool isStream = false]) {
-    _mediaRecorder = html.MediaRecorder(stream);
-    _mediaRecorder?.addEventListener(
-      'dataavailable',
-      isStream ? _onDataAvailableStream : _onDataAvailable,
-    );
-    _mediaRecorder?.addEventListener('stop', _onStop);
-    _mediaRecorder?.start();
-
-    _updateState(RecordState.record);
+  Future<String?> stop(String recorderId) {
+    return _getRecorder(recorderId).stop();
   }
 
   @override
-  Future<bool> isEncoderSupported(AudioEncoder encoder) {
-    final type = _getSupportedMimeType(encoder);
-
-    return Future.value(type != null ? true : false);
+  Future<List<InputDevice>> listInputDevices(String recorderId) {
+    return _getRecorder(recorderId).listInputDevices();
   }
 
   @override
-  Future<Amplitude> getAmplitude() async {
-    // TODO how to check amplitude values on web?
-    return Amplitude(current: -160.0, max: -160.0);
+  Future<bool> isEncoderSupported(String recorderId, AudioEncoder encoder) {
+    return _getRecorder(recorderId).isEncoderSupported(encoder);
   }
 
   @override
+  Future<Amplitude> getAmplitude(String recorderId) {
+    return _getRecorder(recorderId).getAmplitude();
+  }
+
   @override
-  Stream<RecordState> onStateChanged() {
-    _stateStreamCtrl ??= StreamController(
-      onCancel: () {
-        _stateStreamCtrl?.close();
-        _stateStreamCtrl = null;
-      },
-    );
-
-    return _stateStreamCtrl!.stream;
+  Stream<RecordState> onStateChanged(String recorderId) {
+    return _getRecorder(recorderId).onStateChanged();
   }
 
-  String? _getSupportedMimeType(AudioEncoder encoder) {
-    final types = mimeTypes[encoder];
-    if (types == null) return null;
-
-    for (var type in types) {
-      if (html.MediaRecorder.isTypeSupported(type)) {
-        return type;
-      }
-    }
-
-    return null;
-  }
-
-  void _onError(dynamic error) {
-    _resetMediaRecorder();
-    _log(error);
-  }
-
-  void _onDataAvailable(html.Event event) {
-    if (event is html.BlobEvent && event.data != null) {
-      _chunks.add(event.data!);
-    }
-  }
-
-  void _onDataAvailableStream(html.Event event) {
-    final streamCtrl = _recordStreamCtrl;
-    if (streamCtrl == null) return;
-
-    if (event is html.BlobEvent) {
-      final data = event.data;
-      if (data == null) return;
-
-      final fileReader = html.FileReader();
-      fileReader.readAsArrayBuffer(data);
-
-      fileReader.onLoad.listen((event) {
-        final result = fileReader.result;
-
-        if (result is List<int>) {
-          streamCtrl.add(result);
-        }
-      });
-    }
-  }
-
-  void _onStop(html.Event event) {
-    String? audioUrl;
-
-    if (_chunks.isNotEmpty) {
-      final blob = html.Blob(_chunks);
-      audioUrl = html.Url.createObjectUrl(blob);
-    }
-
-    _resetMediaRecorder();
-
-    _onStopCompleter?.complete(audioUrl);
-  }
-
-  void _resetMediaRecorder() {
-    _mediaRecorder?.removeEventListener('dataavailable', _onDataAvailable);
-    _mediaRecorder?.removeEventListener('onstop', _onStop);
-    _mediaRecorder = null;
-
-    final tracks = _mediaStream?.getTracks();
-
-    if (tracks != null) {
-      for (var track in tracks) {
-        track.stop();
-      }
-
-      _mediaStream = null;
-    }
-
-    _chunks = [];
-
-    _recordStreamCtrl?.close();
-    _recordStreamCtrl = null;
-  }
-
-  void _log(String msg) {
-    if (kDebugMode) print(msg);
-  }
-
-  void _updateState(RecordState state) {
-    if (_stateStreamCtrl?.hasListener ?? false) {
-      _stateStreamCtrl?.add(state);
-    }
+  Recorder _getRecorder(String recorderId) {
+    return _recorders[recorderId] != null
+        ? _recorders[recorderId]!
+        : throw PlatformException(
+            code: 'record',
+            message:
+                'Record has not yet been created or has already been disposed.',
+          );
   }
 }
---
+++ record_web/lib/recorder.dart
@@ -0,0 +1,267 @@
+import 'dart:async';
+import 'dart:html' as html;
+
+import 'package:flutter/foundation.dart';
+import 'package:record_platform_interface/record_platform_interface.dart';
+import 'package:record_web/mime_types.dart';
+
+class Recorder {
+  // Media recorder object
+  html.MediaRecorder? _mediaRecorder;
+  // Media stream get from getUserMedia
+  html.MediaStream? _mediaStream;
+  // Audio data
+  List<html.Blob> _chunks = [];
+  // Completer to get data & stop events before `stop()` method ends
+  Completer<String>? _onStopCompleter;
+  StreamController<RecordState>? _stateStreamCtrl;
+  StreamController<List<int>>? _recordStreamCtrl;
+
+  Future<void> dispose() async {
+    _mediaRecorder?.stop();
+    _stateStreamCtrl?.close();
+    _recordStreamCtrl?.close();
+    _resetMediaRecorder();
+  }
+
+  Future<bool> hasPermission() async {
+    final mediaDevices = html.window.navigator.mediaDevices;
+    if (mediaDevices == null) return false;
+
+    try {
+      await mediaDevices.getUserMedia({'audio': true});
+      return true;
+    } catch (_) {
+      return false;
+    }
+  }
+
+  Future<bool> isPaused() async {
+    return _mediaRecorder?.state == 'paused';
+  }
+
+  Future<bool> isRecording() async {
+    return _mediaRecorder?.state == 'recording';
+  }
+
+  Future<void> pause() async {
+    _mediaRecorder?.pause();
+
+    _updateState(RecordState.pause);
+  }
+
+  Future<void> resume() async {
+    _mediaRecorder?.resume();
+
+    _updateState(RecordState.record);
+  }
+
+  Future<void> start(
+    RecordConfig config, {
+    required String path,
+  }) async {
+    return _start(config);
+  }
+
+  Future<Stream<List<int>>> startStream(
+    RecordConfig config,
+  ) async {
+    await _start(config);
+
+    _recordStreamCtrl = StreamController();
+    return _recordStreamCtrl!.stream;
+  }
+
+  Future<String?> stop() async {
+    _onStopCompleter = Completer();
+
+    _mediaRecorder?.stop();
+
+    _updateState(RecordState.stop);
+
+    return _onStopCompleter?.future;
+  }
+
+  Future<List<InputDevice>> listInputDevices() async {
+    final devices = <InputDevice>[];
+
+    final mediaDevices = html.window.navigator.mediaDevices;
+    try {
+      if (mediaDevices == null) {
+        _onError('enumerateDevices() not supported.');
+        return devices;
+      }
+
+      final deviceInfos = await mediaDevices.enumerateDevices();
+      for (var info in deviceInfos) {
+        if (info is html.MediaDeviceInfo &&
+            info.kind == 'audioinput' &&
+            info.deviceId != null &&
+            info.label != null) {
+          devices.add(InputDevice(id: info.deviceId!, label: info.label!));
+        }
+      }
+    } catch (error) {
+      _onError(error);
+    }
+
+    return devices;
+  }
+
+  Future<void> _start(RecordConfig config, [bool isStream = false]) async {
+    _mediaRecorder?.stop();
+    _resetMediaRecorder();
+
+    final constraints = {
+      'audio': true,
+      'audioBitsPerSecond': config.bitRate,
+      'bitsPerSecond': config.bitRate,
+      'sampleRate': config.samplingRate,
+      'channelCount': config.numChannels,
+      if (config.device != null) 'deviceId': {'exact': config.device!.id}
+    };
+
+    // Try to assign dedicated mime type.
+    // If contrainst isn't set, browser will record with its default codec.
+    final mimeType = _getSupportedMimeType(config.encoder);
+    if (mimeType != null) {
+      constraints['mimeType'] = mimeType;
+    }
+
+    try {
+      _mediaStream = await html.window.navigator.mediaDevices?.getUserMedia(
+        constraints,
+      );
+      if (_mediaStream != null) {
+        _onStart(_mediaStream!, isStream);
+      } else {
+        _log('Audio recording not supported.');
+      }
+    } catch (error) {
+      _onError(error);
+    }
+  }
+
+  void _onStart(html.MediaStream stream, [bool isStream = false]) {
+    _mediaRecorder = html.MediaRecorder(stream);
+    _mediaRecorder?.addEventListener(
+      'dataavailable',
+      isStream ? _onDataAvailableStream : _onDataAvailable,
+    );
+    _mediaRecorder?.addEventListener('stop', _onStop);
+    _mediaRecorder?.start();
+
+    _updateState(RecordState.record);
+  }
+
+  Future<bool> isEncoderSupported(AudioEncoder encoder) {
+    final type = _getSupportedMimeType(encoder);
+
+    return Future.value(type != null ? true : false);
+  }
+
+  Future<Amplitude> getAmplitude() async {
+    // TODO how to check amplitude values on web?
+    return Amplitude(current: -160.0, max: -160.0);
+  }
+
+  Stream<RecordState> onStateChanged() {
+    _stateStreamCtrl ??= StreamController(
+      onCancel: () {
+        _stateStreamCtrl?.close();
+        _stateStreamCtrl = null;
+      },
+    );
+
+    return _stateStreamCtrl!.stream;
+  }
+
+  String? _getSupportedMimeType(AudioEncoder encoder) {
+    final types = mimeTypes[encoder];
+    if (types == null) return null;
+
+    for (var type in types) {
+      if (html.MediaRecorder.isTypeSupported(type)) {
+        return type;
+      }
+    }
+
+    return null;
+  }
+
+  void _onError(dynamic error) {
+    _resetMediaRecorder();
+    _log(error);
+  }
+
+  void _onDataAvailable(html.Event event) {
+    if (event is html.BlobEvent && event.data != null) {
+      _chunks.add(event.data!);
+    }
+  }
+
+  void _onDataAvailableStream(html.Event event) {
+    final streamCtrl = _recordStreamCtrl;
+    if (streamCtrl == null) return;
+
+    if (event is html.BlobEvent) {
+      final data = event.data;
+      if (data == null) return;
+
+      final fileReader = html.FileReader();
+      fileReader.readAsArrayBuffer(data);
+
+      fileReader.onLoad.listen((event) {
+        final result = fileReader.result;
+
+        if (result is List<int>) {
+          streamCtrl.add(result);
+        }
+      });
+    }
+  }
+
+  void _onStop(html.Event event) {
+    String? audioUrl;
+
+    if (_chunks.isNotEmpty) {
+      final blob = html.Blob(_chunks);
+      audioUrl = html.Url.createObjectUrl(blob);
+    }
+
+    _resetMediaRecorder();
+
+    _onStopCompleter?.complete(audioUrl);
+  }
+
+  void _resetMediaRecorder() {
+    _mediaRecorder?.removeEventListener('dataavailable', _onDataAvailable);
+    _mediaRecorder?.removeEventListener('onstop', _onStop);
+    _mediaRecorder = null;
+
+    final tracks = _mediaStream?.getTracks();
+
+    if (tracks != null) {
+      for (var track in tracks) {
+        track.stop();
+      }
+
+      _mediaStream = null;
+    }
+
+    _chunks = [];
+
+    _recordStreamCtrl?.close();
+    _recordStreamCtrl = null;
+  }
+
+  void _log(String msg) {
+    if (kDebugMode) print(msg);
+  }
+
+  void _updateState(RecordState state) {
+    if (_stateStreamCtrl?.hasListener ?? false) {
+      _stateStreamCtrl?.add(state);
+    }
+  }
+}
