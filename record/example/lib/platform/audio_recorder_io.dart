96c7ed12f0dee8dce09e8ba0c7209ef7a534d9e4
llfbandit <llfbandit@gmail.com>
2023-10-14 16:30:12 +0200

    chore: Cleanup example to be more readable.

---
+++ record/example/lib/platform/audio_recorder_io.dart
@@ -0,0 +1,42 @@
+import 'dart:io';
+import 'dart:typed_data';
+
+import 'package:path/path.dart' as p;
+import 'package:path_provider/path_provider.dart';
+import 'package:record/record.dart';
+
+mixin AudioRecorderMixin {
+  Future<void> recordFile(AudioRecorder recorder, RecordConfig config) async {
+    final path = await _getPath();
+
+    await recorder.start(config, path: path);
+  }
+
+  Future<void> recordStream(AudioRecorder recorder, RecordConfig config) async {
+    final file = File(await _getPath());
+
+    final stream = await recorder.startStream(config);
+
+    stream.listen(
+      (data) {
+        // ignore: avoid_print
+        print(
+          recorder.convertBytesToInt16(Uint8List.fromList(data)),
+        );
+        file.writeAsBytesSync(data, mode: FileMode.append);
+      },
+      // ignore: avoid_print
+      onDone: () => print('End of stream'),
+    );
+  }
+
+  void downloadWebData(String path) {}
+
+  Future<String> _getPath() async {
+    final dir = await getApplicationDocumentsDirectory();
+    return p.join(
+      dir.path,
+      'audio_${DateTime.now().millisecondsSinceEpoch}.m4a',
+    );
+  }
+}
