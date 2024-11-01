abe62bc6e3509aeacabe03f8b5a117a5ef81ddc9
llfbandit <llfbandit@gmail.com>
2024-11-01 17:38:31 +0100

    chore(example): Add avoid_print lint for all file.

--- record/example/lib/platform/audio_recorder_io.dart
+++ record/example/lib/platform/audio_recorder_io.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: avoid_print
+
 import 'dart:io';
 import 'dart:typed_data';
 
@@ -14,22 +16,19 @@ mixin AudioRecorderMixin {
 
   Future<void> recordStream(AudioRecorder recorder, RecordConfig config) async {
     final path = await _getPath();
-    
+
     final file = File(path);
 
     final stream = await recorder.startStream(config);
 
     stream.listen(
       (data) {
-        // ignore: avoid_print
         print(
           recorder.convertBytesToInt16(Uint8List.fromList(data)),
         );
         file.writeAsBytesSync(data, mode: FileMode.append);
       },
-      // ignore: avoid_print
       onDone: () {
-        // ignore: avoid_print
         print('End of stream. File written to $path.');
       },
     );
