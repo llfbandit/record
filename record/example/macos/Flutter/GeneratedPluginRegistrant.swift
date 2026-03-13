708c2c02292592951b1199c34936003775f38cbd
llfbandit <llfbandit@gmail.com>
2026-03-13 17:52:23 +0100

    fix(macOS): Get away from Flutter UI thread.

--- record/example/macos/Flutter/GeneratedPluginRegistrant.swift
+++ record/example/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,11 +6,9 @@ import FlutterMacOS
 import Foundation
 
 import audioplayers_darwin
-import path_provider_foundation
 import record_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   AudioplayersDarwinPlugin.register(with: registry.registrar(forPlugin: "AudioplayersDarwinPlugin"))
-  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   RecordMacOsPlugin.register(with: registry.registrar(forPlugin: "RecordMacOsPlugin"))
 }
